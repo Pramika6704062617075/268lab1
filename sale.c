8511d2bf408e01d0a7dcba931f6f2ca9b4705338
Pramika6704062617075 <s6704062617075@email.kmutnb.ac.th>
2025-12-09 16:53:45 +0700

    commit 3

--- sale.c
+++ sale.c
@@ -1,25 +1,33 @@
 #include<stdio.h>
 int main (){
 	int a,b,c ;
-	float a1,b1,c1 ,a2,b2,c2 , d, d1 , d2 , e1 , e2, e;
+	float a1 ,a2, d, d1 ,d3,d4, d2 , e1 , e2, e, a3, a4,e3,e4;
 	scanf("%d" ,&a);
 	a1 = a*5/100 ;
 	a2 = a - a1 ;
-	printf("%.2f\n" ,a2);
+	a3 = a*7/100 ;
+	a4 = a2 + a3 ;
+	printf("%.2f\n" ,a4);
 	
 	
 	scanf("%d" ,&b);
 	d = a + b ;
 	d1 = d*15/100 ;
 	d2 = d - d1 ;
-	printf("%.2f\n" ,d2);
+	d3 = d*7/100 ;
+	d4 = d2 + d3 ;
+	printf("%.2f\n" ,d4);
 	
 	
 	scanf("%d" ,&c);
 	e = a+b+c ;
 	e1 = e*30/100 ;
 	e2 = e - e1 ;
-	printf("%.2f\n" ,e2);
+	e3 = e*7/100 ;
+	e4 = e2 + e3 ;
+	printf("%.2f\n" ,e4);
 	
 	return 0 ;
 }
+
+
